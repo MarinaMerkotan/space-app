6ac5d49da84d0df532a83b2eba8588ac42771db5
testName <test@gmail.com>
2024-02-13 11:11:17 +0200

    test6

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -15,7 +15,12 @@ const Home = () => {
     const dateNow = new Date();
     setDate(dateNow.toISOString().substring(0, 10));
   }, []);
+  const wrapperRef = useRef(null);
 
+  const handleClick = () => {
+    console.log('handleClick');
+    wrapperRef?.current?.style?.border = '2px solid red';
+  };
   return (
     <>
       <Head>
@@ -23,14 +28,34 @@ const Home = () => {
       </Head>
       <section className={styles.section_main}>
         <div className={styles.left_block}>
-          <TelegramLoginButton botName='TestCustomButtonBot' dataOnauth={(user) => console.log(user)} />
+          <>
+            <TelegramLoginButton
+              dataOnauth={(user) => {
+                console.log(user);
+              }}
+              botName={'TestCustomButtonBot'}
+              wrapperProps={{
+                ref: wrapperRef,
+                onClick: handleClick,
+              }}
+            />
+          </>
           <h1>Hi, Walle speaking!</h1>
           <p>
             {
               "I will show you how beautiful our universe is. Every day, Nasa posts what it thinks is the best image of the day. To view today's image, just click the button below, or go to the calendar page and choose your date."
             }
           </p>
-          <TelegramButtonOverlay handleTelegramResponse={(user: any) => console.log(user)} />
+          <button
+            onClick={() => {
+              console.log('first');
+              wrapperRef?.current?.onClick();
+            }}
+          >
+            tets click
+          </button>
+
+          {/* <TelegramButtonOverlay handleTelegramResponse={(user: any) => console.log(user)} /> */}
           <div className={styles.wrapper_link}>
             <div className={styles.container}>
               {/* Звичайна зовнішня кнопка */}
@@ -63,53 +88,3 @@ const Home = () => {
 };
 
 export default Home;
-
-const TelegramButtonOverlay = ({ handleTelegramResponse }: {handleTelegramResponse: any}) => {
-  useEffect(() => {
-    // Функція, яка викликається при кліку на вашу власну кнопку
-    const handleCustomButtonClick = () => {
-      // Симулювати клік на кнопку Telegram через скрипт Telegram Widget
-      document.getElementById('telegram-login')!.click();
-    };
-
-    // Додати обробник подій до вашої власної кнопки
-    document.getElementById('custom-button')!.addEventListener('click', handleCustomButtonClick);
-
-    // Повернення функції, яка буде викликана при видаленні компонента
-    return () => {
-      // При видаленні компонента видаляємо обробник подій
-      document.getElementById('custom-button')!.removeEventListener('click', handleCustomButtonClick);
-    };
-  }, []); // Завантажити це тільки раз, після монтування компонента
-
-  return (
-    <>
-      {/* Telegram Widget Button */}
-      <div id='telegram-login' style={{ display: 'none' }}>
-        <script
-          src='https://telegram.org/js/telegram-widget.js?22'
-          data-telegram-login='TestCustomButtonBot'
-          data-size='large'
-          data-request-access='write'
-          data-userpic='false'
-          data-onauth={handleTelegramResponse} // Передаємо обробник
-          async
-        />
-        <iframe
-          id='telegram-login-TestCustomButtonBot'
-          src={`https://oauth.telegram.org/embed/TestCustomButtonBot?origin=${encodeURIComponent(
-            window.location.origin
-          )}&return_to=${encodeURIComponent(window.location.href)}&size=large&userpic=false&request_access=write`}
-          width='238'
-          height='40'
-          frameBorder='0'
-          scrolling='no'
-          style={{ overflow: 'hidden', colorScheme: 'light dark', border: 'none', height: '40px', width: '236px' }}
-        />
-      </div>
-
-      {/* Custom Button */}
-      <button id='custom-button'>Your Button</button>
-    </>
-  );
-};
