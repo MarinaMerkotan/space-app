3ee5f7a72f081cf7091d3f50e66089dac5c71d64
MarinaMerkotan <125581332+MarinaMerkotan@users.noreply.github.com>
2024-08-18 17:49:40 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,12 +6,7 @@ import styles from '@/styles/Home.module.scss';
 import rocket from '../../public/rocket.svg';
 import walle from '../../public/walle.png';
 import Animation from '@/components/Animation';
-import TelegramLoginButton from 'telegram-login-button';
-declare global {
-    interface Window {
-        onTelegramAuth: (user: any) => void;
-    }
-}
+
 const Home = () => {
   const [date, setDate] = useState('');
 
@@ -19,41 +14,6 @@ const Home = () => {
     const dateNow = new Date();
     setDate(dateNow.toISOString().substring(0, 10));
   }, []);
-
-  // useEffect(() => {
-  //   function getParameterByName(name: string) {
-  //     const url = window.location.href;
-  //     console.log('URL', url)
-  //     name = name.replace(/[\[\]]/g, '\\$&');
-  //     const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-  //       results = regex.exec(url);
-  //     if (!results) return null;
-  //     if (!results[2]) return '';
-  //     return decodeURIComponent(results[2].replace(/\+/g, ' '));
-  //   }
-
-  //   const id = getParameterByName('id');
-  //   const first_name = getParameterByName('first_name');
-  //   const last_name = getParameterByName('last_name');
-  //   const username = getParameterByName('username');
-  //   const photo_url = getParameterByName('photo_url');
-  //   const auth_date = getParameterByName('auth_date');
-  //   const hash = getParameterByName('hash');
-
-  //   console.log('ID:', id);
-  //   console.log("first_name:", first_name);
-  //   console.log('last_name:', last_name);
-  //   console.log('username:', username);
-  //   console.log('photo_url:', photo_url);
-  //   console.log('auth_date:', auth_date);
-  //   console.log('hash:', hash);
-  // }, []);
-
-
-    window.onTelegramAuth = user => {
-        alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
-        // Тут ви можете виконати будь-які подальші дії з отриманими даними користувача
-    };
 
   return (
     <>
@@ -68,35 +28,12 @@ const Home = () => {
               "I will show you how beautiful our universe is. Every day, Nasa posts what it thinks is the best image of the day. To view today's image, just click the button below, or go to the calendar page and choose your date."
             }
           </p>
-          <h1>container</h1>
-              <div>
-      {/* Кнопка для виклику скрипта */}
-        <div>
-            {/* Код для вбудовування скрипту Telegram Login Widget */}
-            <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="TestCustomButtonBot" data-size="large" data-userpic="false" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
-            {/* Кнопка для виклику автентифікації через Telegram */}
-            <button
-                onClick={() => {
-                    // Виклик функції, що починає автентифікацію через Telegram
-                    window.loginWithTelegram();
-                }}
-            >
-                Увійти за допомогою Telegram
-            </button>
-        </div>
-
-    </div>
-          <div className={styles.container}>
-            <button className={styles.normalButton}>Button</button>
-
-{/*             <TelegramLoginButton
-              dataOnauth={(user) => console.log(user)}
-              botName={'TestCustomButtonBot'}
-              className={styles.telegramButton}
-              dataAuthUrl='https://space-sigma-three.vercel.app/'
-            /> */}
+          <div className={styles.wrapper_link}>
+            <Link href={`/calendar/${date}`} className={styles.link} prefetch={false}>
+              <Image src={rocket} alt='rocket' width={40} height={40} />
+              {"Today's picture"}
+            </Link>
           </div>
-          <div className={styles.wrapper_link}></div>
         </div>
         <div className={styles.right_block}>
           <Image src={walle} alt='Robot Walle' width={640} height={604} priority />
