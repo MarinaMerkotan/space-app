e363c3eddd15d1841b0fc04423db844bd09c5083
MarinaMerkotan <125581332+MarinaMerkotan@users.noreply.github.com>
2024-08-18 18:43:35 +0300

    Create Loader.tsx

---
+++ src/components/Loader.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from '@/styles/Loader.module.scss';
+
+const Loader = () => {
+  return (
+    <div className={styles.preloader}>
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+    </div>
+  );
+};
+
+export default Loader;
